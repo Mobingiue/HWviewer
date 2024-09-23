777368b5d56b73d688da7686233813d5c9fd635c
Mobingiue <Mobingiue@foxmail.com>
2024-09-24 00:12:16 +0800

    算法初始化

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-        System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(str);
+
     }
 }
