e4d4a53a6db6bc91ebca0e3dda86333321dfa0da
Mobingiue <Mobingiue@foxmail.com>
2024-10-09 18:59:13 +0800

    无

--- src/main/java/HWview1.java
+++ src/main/java/HWview1.java
@@ -4,6 +4,6 @@ package main.java;
 public class HWview1 {
     public static void main(String[] args) {
 
-        System.out.printf("Hello world");
+        System.out.printf("Hello world!");
     }
 }
